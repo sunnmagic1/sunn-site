6a35588089bc3697cddce898a883fa090f546645
Amarachi Obi <thesunnmagic@gmail.com>
2025-05-25 15:03:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Spline scene="https://prod.spline.design/qXcd22ZmbHMd8ebR/scene.splinecode" />
+    </div>
+  );
+}
+
+export default App;
+
+
